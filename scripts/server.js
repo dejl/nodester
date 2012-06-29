478646445e21cf9159a7426495ff99c821534eb4
Alejandro Morales <vamg008@gmail.com>
2012-06-29 00:01:18 -0600

    [dist] Added a warning to users

--- scripts/server.js
+++ scripts/server.js
@@ -1,10 +1,12 @@
 //
-//	      	Nodester 
-//	 Open Source node.js PaaS
+//          Nodester 
+//   Open Source node.js PaaS
 //
 // GNU Affero General Public License 
 //
-
+//
+// PLEASE DO NOT EDIT ANYTHING BELOW THIS LINE 
+// ------------------------------------------
 
 var fs = require('fs')
   , path = require('path')
@@ -12,29 +14,30 @@ var fs = require('fs')
   , coffee = "{{COFFEE}}"
   ;
 
-var iniFile = "{{FILE}}"
+var iniFile = "{{FILE}}";
 
-console.log('')
+console.log('');
 console.log(' [*] ', new Date(),' - Spawing',iniFile);
 console.log(' [*] ', 'Running node-'+ process.version);
 console.log(' [*] ', 'Server now listening on internal port', process.env['app_port'] || 80);
-console.log(' [*] ', 'Public Address:', "{{ADDRESS}}")
+console.log(' [*] ', 'Public Address:', "{{ADDRESS}}");
 
-var pidRunner = path.join(__dirname,'app.pid')
+var pidRunner = path.join(__dirname,'app.pid');
 try {
-	fs.writeFileSync(pidRunner, process.pid, 'utf8');
-	console.log(' [*] ',' Process pid: ', process.pid );
+    fs.writeFileSync(pidRunner, process.pid, 'utf8');
+    console.log(' [*] ',' Process pid: ', process.pid );
 } catch (excp) {
-	console.log(' [*] ', 'Opps... Couldn\'t write my pid to fs (' + process.pid + ')');
+    console.log(' [*] ', 'Opps... Couldn\'t write my pid to fs (' + process.pid + ')');
 }
 
 try {
-	if (coffee != 'false') {
-		require('coffee-script')
-	} 
-	require(__dirname + '/app/'+ iniFile);
+    if (coffee != 'false') {
+        require('coffee-script');
+    } 
+    require(__dirname + '/app/'+ iniFile);
 } catch (excp) {
-	console.error(' [*] ', new Date(), ' -', iniFile, ' don\'t exists' );
-	console.warn(' [*] ', 'Dieing...');
-	return process.kill(process.pid,'SIGINT');
+    console.error(' [*] ', new Date(), ' -', iniFile, ' don\'t exists' );
+    console.warn(' [*] ', 'Dieing...');
+    console.warn(' [*] ', 'Restarting the process');
+    return process.kill(process.pid,'SIGINT');
 }
