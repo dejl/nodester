f439270b50958f84b0bf2124f98fada89f696b4a
Alejandro Morales <vamg008@gmail.com>
2012-06-28 22:57:36 -0600

    [fix] typo ¬¬

--- scripts/chroot_runner.js
+++ scripts/chroot_runner.js
@@ -83,7 +83,7 @@ return !function () {
         console.warn('n is not instaleld');
       } else {
         var nodeVersion ='/usr/local/n/versions/' + version + '/bin/node';
-        var npmVersion = '/usr/local/n/versions/' + versions + '/bin/npm'
+        var npmVersion = '/usr/local/n/versions/' + version + '/bin/npm'
         // export HOME="{{HOME}}"
         // exec {{ENV}} {{VERSION}} {{PATH}}/server.js >> {{PATH}}/logs/app.log
         // Yeah on every start because user can switch versions and stuff
